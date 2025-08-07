e0bec0b7b6a0420957dcf16c62e29a601d3ec76b
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-08-07 15:00:51 +0200

    Initial commit

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#OAR -l nodes=1,walltime=1:0:0
+#OAR -q testing
+#OAR -p larochette
+
+g5k-setup-docker -t
+
+sudo mkdir -p /tmp/ollama /tmp/open-webui
+sudo chown -R 1000:1000 /tmp/ollama /tmp/open-webui
+
+docker compose up
+
