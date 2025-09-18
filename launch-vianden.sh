d4455e92fdd11f3ab3ef8843f9f060897773b836
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2025-09-18 11:16:56 +0200

    launch-vianden.sh - fix hardcoded path

--- launch-vianden.sh
+++ launch-vianden.sh
@@ -5,8 +5,9 @@
 #OAR -t deploy
 #OAR -p vianden
 
-unames=Linux
-unamem=x86_64
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+UNAME_S=Linux
+UNAME_M=x86_64
 
 SSH="ssh -o StrictHostKeyChecking=no root@vianden-1"
 
@@ -17,7 +18,7 @@ $SSH mkdir -p /tmp/docker
 $SSH mkdir -p /var/lib/docker
 $SSH mount --bind /tmp/docker /var/lib/docker
 curl -sSL https://get.docker.com/ | $SSH sh
-$SSH curl -sSL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-${unames}-${unamem}" -o /usr/local/bin/docker-compose
+$SSH curl -sSL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-${UNAME_S}-${UNAME_M}" -o /usr/local/bin/docker-compose
 $SSH chmod +x /usr/local/bin/docker-compose
 $SSH mkdir -p /etc/docker
 echo "{ \"registry-mirrors\": [\"http://docker-cache.grid5000.fr\"] }" | $SSH tee /etc/docker/daemon.json
@@ -29,5 +30,5 @@ $SSH mkdir -p /tmp/ollama /tmp/open-webui
 $SSH chown -R 1000:1000 /tmp/ollama /tmp/open-webui
 
 # Start the docker containers
-$SSH sudo -u $USER -i 'bash -c "cd ~/ollama/ && docker compose up -d"'
-$SSH sudo -u $USER -i 'bash -c "cd ~/ollama/ && docker compose logs -f"'
+$SSH sudo -u $USER -i 'bash -c "cd $WORKING_DIR && docker compose up -d"'
+$SSH sudo -u $USER -i 'bash -c "cd $WORKING_DIR && docker compose logs -f"'
