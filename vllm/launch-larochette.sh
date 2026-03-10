31c57432126b8c0ca9bb5aeac8efa87f2a9fd2fb
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-03-10 16:57:33 +0100

    [vllm] Initial launcher script

---
+++ vllm/launch-larochette.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#OAR -l nodes=1,walltime=1:0:0
+#OAR -p larochette
+
+# Install docker on the compute node
+g5k-setup-docker -t
+
+# Disable NUMA balancing
+sudo sh -c 'echo 0 > /proc/sys/kernel/numa_balancing'
+
+# Start the docker containers
+docker compose up -d
+docker compose logs -f
